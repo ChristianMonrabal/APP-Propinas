5c45d04321bc6d4224d8cc534a5fcad54ef95d98
Christian Monrabal <100007473.joan23@fje.edu>
2022-04-28 18:25:45 +0200

    ultimate configuration

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 function calcProp() {
-    alert("todo ok")
+    var total = document.getElementById('total').value
+    var opciones = document.getElementById('servicio').value
+    var personas = document.getElementById('personas').value
+    var resultado = Number(total) * Number(opciones) / Number(personas)
+    var result = document.getElementById('respuesta')
+    result.innerHTML = "La propina es : " + resultado
 }
